f236e754153ac5b2ca5bd1f5696f673fedb1d3bc
Rn <aryan.sainju@gmail.com>
2019-04-03 22:26:06 +0545

    Initial commit

---
+++ src/boards/templatetags/form_tags.py
@@ -0,0 +1,23 @@
+from django import template
+
+register = template.Library()
+
+''' template filters '''
+
+@register.filter
+def field_type(bound_field):
+    return bound_field.field.widget.__class__.__name__
+
+@register.filter
+def input_class(bound_field):
+    css_class = ''
+    if bound_field.form.is_bound:
+        if bound_field.errors:
+            css_class = 'is-invalid'
+        elif field_type(bound_field) != 'PasswordInput':
+            css_class = 'is-valid'
+    return 'form-control {}'.format(css_class)
+
+
+'''Note that after creating this file, you will have to manually stop the 
+development server and start it again so Django can identify the new template tags.'''
