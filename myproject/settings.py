f65c64246c08f902cf666729e7eaca50b322ef6b
rnsainju <aryan.sainju@gmail.com>
2019-04-06 22:39:42 +0545

    images updated

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,11 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 
 LOGIN_URL = 'login'
