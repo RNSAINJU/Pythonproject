685768e4d6a28b65a509bd122270cdee60d17b26
aryan sainju <aryan.sainju@gmail.com>
2019-05-04 23:23:26 +0545

    added links for static files

--- myproject/settings.py
+++ myproject/settings.py
@@ -146,6 +146,8 @@ MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    '/khwoppagiftcardgaming/static',
+    '/usr/local/lib/python3.6/site-packages/django/contrib/admin/static',
 ]
 
 
