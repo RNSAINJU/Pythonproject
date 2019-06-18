29a2e659900947d9458dfaa1bbb6f52594beb88f
aryan sainju <aryan.sainju@gmail.com>
2019-06-18 14:31:11 +0545

    sd

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rqr_cjv4igscyu8&&(0%e(=sy=f2)p=f_wn&@0xsp7m$@!kp=d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['139.59.95.94','khwoppagiftcard.store','www.khwoppagiftcard.store','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['68.183.91.249','khwoppagiftcard.store','www.khwoppagiftcard.store','localhost','127.0.0.1']
 
 
 # Application definition
