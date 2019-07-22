ac4c190a2b28d9b851a50ff7e84a5bed794bc78e
aryan sainju <aryan.sainju@gmail.com>
2019-07-22 23:58:44 +0545

    admin changes

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import OrderProduct, Order, Payment, Coupon,OrderDetail
 
+# class OrderPayment(admin.StackedInline):
+#     model=OrderDetail
+
 class OrderAdmin(admin.ModelAdmin):
-    list_display=['id','user','ordered','ordered_date','status']
+    # inlines=[OrderPayment]
+    list_display=['id','user','ordered','ordered_date','payment','order_details','status']
+    list_display_links=('id','user')
+    # list_editable=('',)
+    list_filter=('ordered','ordered_date','status')
+    # search_fields= ('user',)
+
 
 admin.site.register(OrderProduct)
 admin.site.register(Order, OrderAdmin)
--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from .models import Product, ChildProduct,Main_Category
+from .models import *
 
-admin.site.register(Product)
-admin.site.register(ChildProduct)
-admin.site.register(Main_Category)
-# admin.site.register(Partner)
-# Register your models here.
+
+class Categories(admin.StackedInline):
+    model=ChildProduct
+    extra =1
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines=[Categories]
+    list_display=['id','title','short_description','description','category','status']
+    list_display_links=('id','title')
+    list_editable=('status',)
+    list_filter=('category','status')
+    # search_fields= ('user',)
+
+class ChildProductAdmin(admin.ModelAdmin):
+    list_display=['type','price','discount_price','status','homefeatured','productsfeatured','enabledetail']
+    list_editable=('price','discount_price','status','homefeatured','productsfeatured','enabledetail')
+    list_filter=('parent_product','status')
+
+# admin.site.register(Main_Category)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(ChildProduct,ChildProductAdmin)
