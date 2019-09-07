e416268275913a87b6ffa566cf7bfe4e47a5e7cf
aryan sainju <aryan.sainju@gmail.com>
2019-09-07 14:05:25 +0545

    ss

--- myproject/settings.py
+++ myproject/settings.py
@@ -206,16 +206,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_URL = '/static_root/'
+STATIC_URL = '/static/'
 MEDIA_URL ='/media/'
 #
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static_in_env'),
     ]
 
-STATIC_ROOT= os.path.join(os.path.dirname(BASE_DIR), "static","static_root")
+# STATIC_ROOT= os.path.join(os.path.dirname(BASE_DIR), "static","static_root")
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 # MEDIA_ROOT= os.path.join(os.path.dirname(BASE_DIR), "static","media_root")
